73a92bf6a5ea718f6f351b3da2bdb47cbe23f89d
LeonSilva15 <l30silva.o@gmail.com>
2023-05-14 19:20:25 -0600

    docs: rename and add comments to exponentiation by squaring

--- exp_by_sqr.py
+++ exp_by_sqr.py
@@ -1,16 +1,18 @@
-count = 0
-def exp( x, n):
-    global count
+ops = 0
+def exp( x, n ):
+    global ops
+
     # Base case
     if ( n == 1 ):
-        print( x )
         return x
+
     # Even
     if ( n % 2 == 0 ):
-        count += 1
+        ops += 1
         return exp( x * x, n // 2 )
+
     # Odd
-    count += 2
+    ops += 2
     return x * exp( x * x, n // 2 )
 
-print( exp( 2, 100 ), count )
+print( exp( 10, 5 ), ops )
